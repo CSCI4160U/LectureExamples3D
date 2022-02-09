9667dbfdd86902b4509a0671fbb29e6513924c25
randyfortier <randy.fortier@uoit.ca>
2022-02-09 11:04:30 -0500

    Added materials, textures, and lights demo

---
+++ Assets/Scripts/AutoRotator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour {
+    [SerializeField] private float rotationSpeed = 1f;
+
+    private float rotationAngle = 0f;
+
+    void Update() {
+        rotationAngle += rotationSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+}
