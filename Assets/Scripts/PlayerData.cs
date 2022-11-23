f756518eadee89f89bd1c31b3fe5736a977117a2
randyfortier <randy.fortier@uoit.ca>
2022-11-23 14:00:36 -0500

    Created new empty project as a starting point for 3D examples

--- Assets/Scripts/PlayerData.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class PlayerData {
-    public Vector3 playerPosition;
-} 
--- Assets/Scripts/WeaponInventory.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class WeaponInventory : MonoBehaviour {
-    public bool activeWeaponExists = false;
-    public int ammunition = 0;
-}
